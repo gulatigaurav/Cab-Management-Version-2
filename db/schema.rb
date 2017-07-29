fe2be9b146db7781cad76330a69be9cf7d0e29be
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-07-29 10:50:41 +0530

    Integrate font awesome icons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728152604) do
+ActiveRecord::Schema.define(version: 20170729025432) do
 
   create_table "cabs", force: :cascade do |t|
     t.string   "name"
