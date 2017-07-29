897ce2297a97c3b43ed7a056cb442ca9083b0f67
Gaurav Gulati <gauravgulati9c@gmail.com>
2017-07-29 11:33:13 +0530

    Student sign up and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :students
   devise_for :users
   root 'welcome#index'
   resources :cabs
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170729025432) do
+ActiveRecord::Schema.define(version: 20170729053453) do
 
   create_table "cabs", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,27 @@ ActiveRecord::Schema.define(version: 20170729025432) do
     t.string   "source"
     t.string   "destination"
     t.string   "via"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_students_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
